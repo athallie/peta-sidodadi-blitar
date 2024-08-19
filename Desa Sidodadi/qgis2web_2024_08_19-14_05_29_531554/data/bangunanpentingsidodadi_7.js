f51105251b9390e3dd0226a75976d36738af212a
eavelie <athallahnaufalr@gmail.com>
2024-08-19 14:09:57 +0700

    added web map

---
+++ Desa Sidodadi/qgis2web_2024_08_19-14_05_29_531554/data/bangunanpentingsidodadi_7.js
@@ -0,0 +1,1 @@
+var json_bangunanpentingsidodadi_7 = {"type":"FeatureCollection","name":"bangunanpentingsidodadi_7","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"nama":"Kantor Desa Sidodadi"},"geometry":{"type":"Point","coordinates":[112.246197242403611,-8.039134128278995]}}]}
